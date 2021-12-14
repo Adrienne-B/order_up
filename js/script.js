87d42bc6f701ee2920289eedae7540ec39e8dfae
adrienne_b <WorldofArts_95@yahoo.com>
2021-12-14 06:48:59 -0600

    Added arrow and submit buttons at the bottom of the page.

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ themeSwitchInput.addEventListener("click", function () {
     if (body.classList.contains("light")) {
       body.classList.remove("light");
       ball.classList.remove("move-right");
-;      //h1.innerText = "Stealth Quincy";
+     //h1.innerText = "Stealth Quincy";
     } else {
       body.classList.add("light");
       ball.classList.add("move-right");
@@ -60,39 +60,44 @@ themeSwitchInput.addEventListener("click", function () {
 
 
 
-  var cheeseBurger = 10;
-  var baconDeluxe = 5;
-  var doubleBig = 8;
+ // var cheeseBurger = 10;
+ // var baconDeluxe = 5;
+ // var doubleBig = 8;
 
-  var grilledChicken = 6;
-  var doubleCheeseChicken = 18;
-  var doubleDeluxeChicken = 4;
+ // var grilledChicken = 6;
+  //var doubleCheeseChicken = 18;
+  //var doubleDeluxeChicken = 4;
 
-  var fishWithCheese = 7;
-  var deluxeFish = 5;
+  //var fishWithCheese = 7;
+  //var deluxeFish = 5;
 
   
-  if (time < 12) {
-    alert("Good morning, welcome to South Rouxe, how may I help you?");
+//  if (time < 12) {
+  //  alert("Good morning, welcome to South Rouxe, how may I help you?");
 
-  } else if (time <= 18) {
-    prompt("Good afternoon, we are serving lunch, how may I help you?");
-  } else if (cheeseBurger <= 18) {
-    alert(`We have ${cheeseBurger} cheese burgers left.`);
-  } else if (baconDeluxe <= 4) {
-    alert(`We have ${baconDeluxe} bacon deluxe burgers left.`);
-  } else if (doubleBig <= 6) {
-    alert(`We have ${doubleBig} double big cheese burgers left.`);
-  } else if (grilledChicken <= 6) {
-    alert(`We have ${grilledChicekne}grilled chicken sandwiches left.`);
-  } else if (doubleDeluxeChicken <= 3) {
-    alert(`We have ${doubleDeluxeChicken} double deluxe chicken sandwiches left.`);
-  } else if (fishWithCheese <= 4) {
-    alert(`We have ${fishWithCheese} fish with cheese sandwishes left.`);
-  } else if (deluxeFish <= 4) {
-    alert(`We have ${deluxeFish}deluxe fish sandwishes left.`);
-  } else if (time <= 18) {
-    prompt(`Good afternoon, welcome to South Rouxe, we are serving lunch, how may I help you?`);
-  } else {
-    alert("Please come to the front to get your order.");
-  }
+ // } else if (time <= 18) {
+  //  prompt("Good afternoon, we are serving lunch, how may I help you?");
+  //} else if (cheeseBurger <= 18) {
+ //   alert(`We have ${cheeseBurger} cheese burgers left.`);
+  //} else if (baconDeluxe <= 4) {
+ //   alert(`We have ${baconDeluxe} bacon deluxe burgers left.`);
+ // } else if (doubleBig <= 6) {
+  //  alert(`We have ${doubleBig} double big cheese burgers left.`);
+  //} else if (grilledChicken <= 6) {
+  //  alert(`We have ${grilledChicekne}grilled chicken sandwiches left.`);
+  //} else if (doubleDeluxeChicken <= 3) {
+ //   alert(`We have ${doubleDeluxeChicken} double deluxe chicken sandwiches left.`);
+  //} else if (fishWithCheese <= 4) {
+  //  alert(`We have ${fishWithCheese} fish with cheese sandwishes left.`);
+  //} else if (deluxeFish <= 4) {
+  //  alert(`We have ${deluxeFish}deluxe fish sandwishes left.`);
+  //} else if (time <= 18) {
+  //  prompt(`Good afternoon, welcome to South Rouxe, we are serving lunch, how may I help you?`);
+  //} else {
+   // alert("Please come to the front to get your order.");
+ // }
+
+
+
+
+
